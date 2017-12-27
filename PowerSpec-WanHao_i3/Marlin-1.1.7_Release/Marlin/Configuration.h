d7ce5cb7cb2a05c1f7bac08f5ba12112e20dbb2c
jonathanweinberg <jonathanweinberg@noreply.github.com>
2017-12-27 18:38:48 -0500

    whoops...little too big

--- PowerSpec-WanHao_i3/Marlin-1.1.7_Release/Marlin/Configuration.h
+++ PowerSpec-WanHao_i3/Marlin-1.1.7_Release/Marlin/Configuration.h
@@ -780,8 +780,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 210
-#define Y_BED_SIZE 210
+#define X_BED_SIZE 200
+#define Y_BED_SIZE 200
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
